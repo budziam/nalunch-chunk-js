53768a2f9eec562759ccffa6f55e9a565fceb740
Michał Budziak <michal.mariusz.b@gmail.com>
2019-07-19 18:50:23 +0200

    Test tsconfig

--- __tests__/factories.test.ts
+++ __tests__/factories.test.ts
@@ -3,6 +3,7 @@ import { ChunkCollectionStore, createChunkCollectionStore } from "../src";
 describe("factories", () => {
     it("creates chunk collection store", () => {
         // when
+
         const chunkCollectionStore = createChunkCollectionStore();
 
         // then
