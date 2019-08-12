30f3c4038cfa8d0b42af1727baeaa616fa2f4233
Michał Budziak <michal.mariusz.b@gmail.com>
2019-08-12 11:48:57 +0200

    Use slugify library

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,13 @@
-import { Coordinates, Dict } from "./types";
+import { Coordinates } from "./types";
+// tslint:disable-next-line:match-default-export-name
+import baseSlugify from "slugify";
 
 export const stringifyCoordinates = (coordinates: Coordinates, separator: string = ","): string =>
     [coordinates.latitude, coordinates.longitude].join(separator);
 
 export const trimSlashes = (text: string): string => text.replace(/^\/+/, "").replace(/\/+$/, "");
 
-const SPECIAL_CHARS_MAPPING: Dict<string> = {
-    ł: "l",
-    Ł: "L",
-};
-
-// https://stackoverflow.com/questions/286921/efficiently-replace-all-accented-characters-in-a-string/9667817#9667817
-export const replaceDiactricChars = (text: string): string => {
-    try {
-        return text
-            .normalize("NFD")
-            .replace(/[\u0300-\u036f]/g, "")
-            .replace(/[^A-Za-z0-9]/g, char => SPECIAL_CHARS_MAPPING[char] || char);
-    } catch (e) {
-        console.error(e);
-        return text;
-    }
-};
-
-export const slugify = (text: string): string =>
-    replaceDiactricChars(text)
-        .toLowerCase()
-        .trim()
-        .replace(/[^A-Za-z0-9]+/g, "-");
+export const slugify = (text: string): string => baseSlugify(text).toLowerCase();
 
 export const nullToUndefined = <T>(value: T): Exclude<T, null> | undefined =>
     (value === null ? undefined : value) as Exclude<T, null>;
